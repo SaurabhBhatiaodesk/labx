6bbad60a3ae6ee428b8c5cd23c890160d4166f4b
SaurabhBhatiaodesk <117754991+SaurabhBhatiaodesk@users.noreply.github.com>
2024-11-06 19:15:48 +0530

    Update OurClient.tsx

--- src/components/HomeCpmponents/OurClients/OurClient.tsx
+++ src/components/HomeCpmponents/OurClients/OurClient.tsx
@@ -123,7 +123,7 @@ export default function Productswiper({}) {
               </SwiperSlide>
             </Swiper>
           </div>
-          <div className="custom-navigation">
+{/*           <div className="custom-navigation">
           <button onClick={handlePrev} className="prev-button bg-gray-300 p-4 rounded-full">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
               <path d="M15 18L9 12L15 6" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -134,7 +134,7 @@ export default function Productswiper({}) {
               <path d="M9 18L15 12L9 6" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
           </button>
-        </div>
+        </div> */}
         </div>
       </div>
     </div>
