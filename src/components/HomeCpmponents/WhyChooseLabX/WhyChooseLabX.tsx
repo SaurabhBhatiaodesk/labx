c82e2b28ff6fa2c5577b1b91625f81af58eff372
SaurabhBhatiaodesk <117754991+SaurabhBhatiaodesk@users.noreply.github.com>
2024-11-11 18:26:35 +0530

    Update WhyChooseLabX.tsx

--- src/components/HomeCpmponents/WhyChooseLabX/WhyChooseLabX.tsx
+++ src/components/HomeCpmponents/WhyChooseLabX/WhyChooseLabX.tsx
@@ -18,7 +18,7 @@ const WhyChooseLabX: React.FC = () => {
     // <div>WhyChooseLabX</div>
 
     <>
-      <div>
+       <div>
         <div className="container" data-aos="fade-up">
           <MainHeading Heading="Why Choose LabX Repair?" />
           <MainTitle Title="Please securely package your phone and ship it to us at the designated address. Make sure to include any required documentation for efficient processing." />
@@ -27,60 +27,49 @@ const WhyChooseLabX: React.FC = () => {
             <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 justify-center text-center gap-4">
               <div className="p-[20px] rounded-[15px] border-2 border-[#ede574] shadow-lg shadow-[#ede57456] hover:shadow-[0_0_20px_10px_#ede57456] transition-shadow duration-300">
                 <div className="flex justify-center">
-                  <Image className="pb-2" src={Repairs} alt="" />
+                  <Image className="pb-2" src={Repairs} alt="Repairs" />
                 </div>
                 <h3 className="text-[#EDE574] lg:w-[70%] m-auto">
                   Expertise in Complex Repairs
                 </h3>
                 <p>
-                  We excel in fixing challenging issues that others can't
-                  handle, including intricate microsoldering and motherboard
-                  repair services. When others can't succeed, we find a
-                  solution.
+                  We excel in fixing challenging issues that others can&apos;t handle, including intricate microsoldering and motherboard repair services. When others can&apos;t succeed, we find a solution.
                 </p>
               </div>
 
               <div className="p-[20px] rounded-[15px] border-2 border-[#ede574] shadow-lg shadow-[#ede57456] hover:shadow-[0_0_20px_10px_#ede57456] transition-shadow duration-300">
                 <div className="flex justify-center">
-                  <Image
-                    className="pb-2"
-                    src={Transparency}
-                    alt="Transparency"
-                  />
+                  <Image className="pb-2" src={Transparency} alt="Transparency" />
                 </div>
                 <h3 className="text-[#EDE574] lg:w-[70%] m-auto">
                   Complete Transparency in Our Process
                 </h3>
                 <p>
-                  Our grading and repair process is 100% transparent.
-                  You&apos;ll receive photos and detailed reports for any repair
-                  decisions, so you always know what&apos;s happening.
+                  Our grading and repair process is 100% transparent. You&apos;ll receive photos and detailed reports for any repair decisions so you always know what&apos;s happening.
                 </p>
               </div>
+
               <div className="p-[20px] rounded-[15px] border-2 border-[#ede574] shadow-lg shadow-[#ede57456] hover:shadow-[0_0_20px_10px_#ede57456] transition-shadow duration-300">
                 <div className="flex justify-center">
-                  <Image className="pb-2" src={Recovery} alt="B2BServices" />
+                  <Image className="pb-2" src={Recovery} alt="Recovery" />
                 </div>
                 <h3 className="text-[#EDE574] lg:w-[70%] m-auto">
                   Specialized Data Recovery
                 </h3>
                 <p>
-                  Even if your device is non-functional, we employ advanced
-                  techniques to recover your important data, providing a
-                  valuable opportunity to retrieve lost files.
+                  Even if your device is non-functional, we employ advanced techniques to recover your important data, providing a valuable opportunity to retrieve lost files.
                 </p>
               </div>
-              <div className="p-[20px] rounded-[15px] border-2 border-[#ede574] shadow-lg shadow-[#ede57456] hover:shadow-[0_0_20px_10px_#ede57456] transition-shadow duration-300">
+
+              <div className="p-[20px] rounded-[8px] border-2 border-[#ede574] shadow-lg shadow-[#ede57456] hover:shadow-[0_0_20px_10px_#ede57456] transition-shadow duration-300">
                 <div className="flex justify-center">
-                  <Image className="pb-2" src={B2BServices} alt="B2BServices" />
+                  <Image className="pb-2" src={B2BServices} alt="B2B Services" />
                 </div>
                 <h3 className="text-[#EDE574] lg:w-[70%] m-auto">
                   Customized B2B Services
                 </h3>
                 <p>
-                  LabX Repair offers tailored solutions for businesses,
-                  including bulk refurbishments and wholesale repairs, ensuring
-                  your operations run smoothly and efficiently.
+                  LabX Repair offers tailored solutions for businesses, including bulk refurbishments and wholesale repairs, ensuring your operations run smoothly and efficiently.
                 </p>
               </div>
             </div>
