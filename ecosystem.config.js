0ae6e6d9801b0841bc9beb69d36e408675ef12a5
brijeshbase2brand <brijeshbase2brand@gmail.com>
2024-11-14 18:04:48 +0530

    mail changes done3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,35 @@
+// module.exports = {
+//   apps: [{
+//     name: "my-nextjs-app",
+//     script: "npm",
+//     args: "start",
+//     watch: true,
+//     env: {
+//       NODE_ENV: "development",
+//     },
+//     env_production: {
+//       NODE_ENV: "production",
+//     }
+//   }]
+// };
+
+
+
 module.exports = {
   apps: [{
     name: "my-nextjs-app",
-    script: "npm",
-    args: "start",
-    watch: true,
+    script: "node_modules/next/dist/bin/next",  // Start Next.js directly
+    args: "start",  // Production start command for Next.js
+    watch: false,  // Disable watching for production
     env: {
       NODE_ENV: "development",
     },
     env_production: {
       NODE_ENV: "production",
-    }
+      NEXT_PUBLIC_API_URL: "https://labxrepair.com.au",  // Example environment variable
+    },
+    out_file: "/var/log/my-nextjs-app/out.log",
+    error_file: "/var/log/my-nextjs-app/error.log",
+    log_date_format: "YYYY-MM-DD HH:mm Z",  // Log date format for better readability
   }]
 };
