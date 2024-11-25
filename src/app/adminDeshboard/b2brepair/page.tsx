bbf1cbc82abc8ab21a36c32832f3564d6451918d
SaurabhBhatiaodesk <s.bhatiaodesk@gmail.com>
2024-11-25 19:27:24 +0530

    added space

--- src/app/adminDeshboard/b2brepair/page.tsx
+++ src/app/adminDeshboard/b2brepair/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import PageTitle from "@/components/Commen/PageTitle";
 import B2BRepairTable from "@/components/adminDeshboard/B2BRepair/B2BRepairTable";
 
+
 export default function Page() {
   return (
     <div className="container ">
