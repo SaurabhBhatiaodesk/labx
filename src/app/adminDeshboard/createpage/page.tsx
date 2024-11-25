cd3516ecdee614940184200b31e03c92122b720d
SaurabhBhatiaodesk <s.bhatiaodesk@gmail.com>
2024-11-25 13:44:24 +0530

    fixed on submit issue

--- src/app/adminDeshboard/createpage/page.tsx
+++ src/app/adminDeshboard/createpage/page.tsx
@@ -14,6 +14,7 @@ export default function AddPage() {
     router.push('/adminDeshboardList');
   };
 
+
   return (
     <div style={{ padding: '20px', backgroundColor: '#c4ced7' }}>
       <h2>Add Page</h2>
