f1bad2852a3056c888286e43ab5952013ab929cb
SaurabhBhatiaodesk <s.bhatiaodesk@gmail.com>
2024-11-28 01:48:50 +0530

    Code revert to worked build.

--- next.config.mjs
+++ next.config.mjs
@@ -10,11 +10,8 @@ const nextConfig = {
     webpack(config) {
       // Disable CSS minification to avoid issues with special characters like '/'
       config.optimization.minimize = false;
-  
-      // Add custom Webpack rules or resolve any other issues if necessary
       return config;
     },
   };
-  
+
   export default nextConfig;
-  
