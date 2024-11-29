f8c648fe1bf48d8dd066742d1c6226fa3e1b8d2a
SaurabhBhatiaodesk <s.bhatiaodesk@gmail.com>
2024-11-29 13:44:09 +0530

    Allow s3 url in next config file.

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,17 @@
 
 // export default nextConfig;
 
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["labxbackend.s3.us-east-2.amazonaws.com"], // Add your S3 bucket domain here
+  },
   webpack(config, { isServer }) {
     if (!isServer) {
       config.optimization.minimize = false;
     }
     return config;
-  }
+  },
 };
 
 export default nextConfig;
