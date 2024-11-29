2864b22924d811163d28896f8937449fa87ee00b
SaurabhBhatiaodesk <s.bhatiaodesk@gmail.com>
2024-11-29 17:21:53 +0530

    git pullof main in image-upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     images: {
       domains: ['labxbackend.s3.us-east-2.amazonaws.com'], // Add your S3 bucket domain here
     },
   };
 
   export default nextConfig;
-=======
-  images: {
-    domains: ["labxbackend.s3.us-east-2.amazonaws.com"], // Add your S3 bucket domain here
-  },
-};
-
-export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["labxbackend.s3.us-east-2.amazonaws.com"], // Add your S3 bucket domain here
-//   },
-//   webpack(config, { isServer }) {
-//     if (!isServer) {
-//       config.optimization.minimize = false;
-//     }
-//     return config;
-//   },
-// };
-
-// export default nextConfig;
->>>>>>> 31c11e323d7f42a3fa92dd85d59fb27c021fdbfd
