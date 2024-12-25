6454a9d164ef2410ab00d04360a0c6184899981a
brijeshbase2brand <brijeshbase2brand@gmail.com>
2024-12-25 20:22:57 +0530

    deleted all files

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['labxbackend.s3.us-east-2.amazonaws.com'], // Add your S3 bucket domain here
-    },
-  };
-
-  export default nextConfig;
