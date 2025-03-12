ef1f9731798727c8260671dc0a75671c5db899cd
techbase2brand <techbase2brand@gmail.com>
2025-03-12 17:34:55 +0530

    3 time

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,6 +103,7 @@ export default function RootLayout({
         />
         {/* Google Tag Manager (GTM) */}
         <Script
+        id="your-unique-id" 
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
