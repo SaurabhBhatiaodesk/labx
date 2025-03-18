c2d597bd9da546e8500e20537c825e39e5a87eae
techbase2brand <techbase2brand@gmail.com>
2025-03-18 12:19:33 +0530

    Meta Pixel Code Clarity Script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,8 +41,8 @@
 // }
 
 import "../Styles/globals.css";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
+// import Header from "@/components/Header/Header";
+// import Footer from "@/components/Footer/Footer";
 import { Space_Grotesk, Poppins } from "next/font/google";
 import ConditionalWrapper from "@/components/ConditionalWrapper"; // New client-side wrapper component
 import { Providers } from "./redux/providers";
@@ -103,7 +103,7 @@ export default function RootLayout({
         />
         {/* Google Tag Manager (GTM) */}
         <Script
-        id="your-unique-id" 
+          id="your-unique-id"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -113,7 +113,51 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-TGGL6B3L');`,
           }}
         />
-        
+
+        {/* Meta Pixel Code */}
+        <Script
+          id="Meta_Pixel_Code"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s) {
+                if(f.fbq)return;n=f.fbq=function(){
+                  n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)
+                };
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];
+                t=b.createElement(e);t.async=!0; t.src=v;
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1179806136863221');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1179806136863221&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
+        {/* Clarity Script */}
+        <Script
+          id="Clarity_Script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qpj0tgz5sa");
+            `,
+          }}
+        />
       </body>
     </html>
   );
