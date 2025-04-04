c496920c562b2411fb1bd5f9563a6493e3df3c02
techbase2brand <techbase2brand@gmail.com>
2025-04-03 23:58:09 -0700

    banner code remove

--- src/app/PS5_repairs/PS5Banner.tsx
+++ src/app/PS5_repairs/PS5Banner.tsx
@@ -1,34 +1,13 @@
-"use client";
-import Image from "next/image";
-
-
-
 
 import React from "react";
-
 import "swiper/css";
-import "swiper/css/pagination";
-// import AOS from "aos";
-import "aos/dist/aos.css";
-import { Pagination } from "swiper/modules";
 import Link from "next/link";
-
 import PSBanner from "../../../public/Images/BannerImages/PS5.webp";
 import MainButton from "@/components/MainButton/MainButton";
 
-
 export default function PS5Banner() {
-
-
-
-
-
-
-
-
   return (
     <>
-    
       <section
         className="background-banner  h-[70vh]"
         style={{
@@ -39,50 +18,34 @@ export default function PS5Banner() {
         }}
       >
         <div className="container">
-          
-        
-            {/* Slide 1 */}
-        
-              <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 items-center xl:py-5">
-                <div>
-                <div className="md:mb-16 mb-11 lg:mt-[0] mt-[0px] relative">
-            <Link href="/training">
-              <button
-                className="px-[20px] py-[6px] border-[2px] rounded-full bg-[rgba(157,156,152,0.83)] shadow-[5px_4px_28px_1px_#9d9c98]
-    animate-pulse transition-all duration-300 hover:shadow-[0_0_25px_10px_rgba(255,223,101,0.8)] md:text-[18px] text-[12px] tracking-[1.5px] relative top-6"
-                style={{ transform: "rotate(354deg)" }} // Equivalent to -5deg (360 - 5)
-              >
-              WHEN YOU NEED IT FIXED, NOW
-              </button>
+          {/* Slide 1 */}
 
-            </Link>
-          </div>
-                  <h1 className="2xl:text-[2.6rem] text-[1.6rem] font-bold text-white tracking-[1.5px] leading-tight bg-gradient-to-r to-black py-5 transition-opacity duration-1000">
-                  
-                    <span className="text-[#EDE574]">
-                    
-                      PS5  {" "}
-                    </span>
-                    REPAIRS  
-                  </h1>
-                  <p>
-                    {`LabXrepair, powered by Bharat, is a mobile phone repair business that will provide various services and training through a dynamic and responsive website and mobile app. The project will be divided into phases, starting with a basic live launch and expanding over time to include full functionalities.`}
-                  </p>
-                  <div className="my-4 xl:my-10">
-                    <MainButton
-                      MainButton="Request a Quote"
-                      link="/contact-us"
-                    />
-                  </div>
-                </div>
-                <div className="w-full">
-                 
-                </div>
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 items-center xl:py-5">
+            <div>
+              <div className="md:mb-16 mb-11 lg:mt-[0] mt-[0px] relative">
+                <Link href="/training">
+                  <button
+                    className="px-[20px] py-[6px] border-[2px] rounded-full bg-[rgba(157,156,152,0.83)] shadow-[5px_4px_28px_1px_#9d9c98]
+    animate-pulse transition-all duration-300 hover:shadow-[0_0_25px_10px_rgba(255,223,101,0.8)] md:text-[18px] text-[12px] tracking-[1.5px] relative top-6"
+                    style={{ transform: "rotate(354deg)" }} // Equivalent to -5deg (360 - 5)
+                  >
+                    WHEN YOU NEED IT FIXED, NOW
+                  </button>
+                </Link>
               </div>
-     
-           
-         
-      
+              <h1 className="2xl:text-[2.6rem] text-[1.6rem] font-bold text-white tracking-[1.5px] leading-tight bg-gradient-to-r to-black py-5 transition-opacity duration-1000">
+                <span className="text-[#EDE574]">PS5 </span>
+                REPAIRS
+              </h1>
+              <p>
+                {`LabXrepair, powered by Bharat, is a mobile phone repair business that will provide various services and training through a dynamic and responsive website and mobile app. The project will be divided into phases, starting with a basic live launch and expanding over time to include full functionalities.`}
+              </p>
+              <div className="my-4 xl:my-10">
+                <MainButton MainButton="Request a Quote" link="/contact-us" />
+              </div>
+            </div>
+            <div className="w-full"></div>
+          </div>
         </div>
       </section>
     </>
