5d7c36b711a245d5752e84f5310d2d56c9aeb377
techbase2brand <techbase2brand@gmail.com>
2025-04-15 13:53:12 +0530

    snap chat pixel added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -161,6 +161,26 @@ export default function RootLayout({
             `,
           }}
         />
+         <Script id="snap-pixel" strategy="afterInteractive">
+        {`
+          (function(e,t,n){
+            if(e.snaptr) return;
+            var a=e.snaptr=function(){
+              a.handleRequest ? a.handleRequest.apply(a,arguments) : a.queue.push(arguments)
+            };
+            a.queue=[];
+            var s='script';
+            var r=t.createElement(s);
+            r.async=!0;
+            r.src=n;
+            var u=t.getElementsByTagName(s)[0];
+            u.parentNode.insertBefore(r,u);
+          })(window,document,'https://sc-static.net/scevent.min.js');
+
+          snaptr('init', 'cb889d03-2e36-47e3-93ef-d7b3ece4f952', {});
+          snaptr('track', 'PAGE_VIEW');
+        `}
+      </Script>
       </body>
     </html>
   );
